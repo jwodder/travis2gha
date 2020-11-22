60c088514a5a95259b63525d3ee84a1e6c66ac5c
John T. Wodder II <git@varonathe.org>
2020-11-21 20:22:40 -0500

    Packaging

---
+++ src/travis2gha/__init__.py
@@ -0,0 +1,11 @@
+"""
+Ad hoc script for converting my Python projects from Travis to GitHub Actions
+
+Visit <https://github.com/jwodder/travis2gha> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'travis2gha@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/travis2gha'
